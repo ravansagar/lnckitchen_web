3c16566fa2de17aca7b289c691406480486602f7
ravansagar <huntersagar08@gmail.com>
2024-06-30 01:39:35 +0545

    "Refactored FetchRandom component: updated imports, reorganized code structure, and modified logic for fetching and processing data"

--- src/components/DynamicHomePage/FetchRandom.jsx
+++ src/components/DynamicHomePage/FetchRandom.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'eact';
 import { get, ref } from 'firebase/database';
 import { database } from 'components/LoginSignUp/firebaseConfig';
 
@@ -7,7 +7,7 @@ const useFetchRandomFoods = (user) => {
     const [dataLoaded, setDataLoaded] = useState(false);
     const [cartFoods, setCartFoods] = useState([]);
     const [orderFoods, setOrderFoods] = useState([]);
-
+  
     useEffect(() => {
         let isMounted = true;
 
@@ -21,84 +21,103 @@ const useFetchRandomFoods = (user) => {
                 const dishesRef = ref(database, 'Dishes');
                 const snapshot = await get(dishesRef);
 
-                const descCartsRef = ref(database, `carts/${user.uid}`);
-                const snapshot2 = await get(descCartsRef);
+                if (user) {
+                    const descCartsRef = ref(database, `carts/${user.uid}`);
+                    const snapshot2 = await get(descCartsRef);
 
-                const descOrdersRef = ref(database, `transactions/${user.uid}`);
-                const snapshot3 = await get(descOrdersRef);
+                    const descOrdersRef = ref(database, `transactions/${user.uid}`);
+                    const snapshot3 = await get(descOrdersRef);
 
-                if (snapshot.exists()) {
-                    const allDishes = snapshot.val();
-                    const allFoods = Object.keys(allDishes).reduce((acc, category) => [
-                        ...acc,
-                        ...allDishes[category].items,
-                    ], []);
+                    if (snapshot.exists()) {
+                        const allDishes = snapshot.val();
+                        const allFoods = Object.keys(allDishes).reduce((acc, category) => [
+                           ...acc,
+                           ...allDishes[category].items,
+                        ], []);
 
-                    let cartDescriptions = [];
-                    let orderDescriptions = [];
+                        let cartDescriptions = [];
+                        let orderDescriptions = [];
 
-                    if (snapshot2.exists()) {
-                        const cartData = snapshot2.val();
-                        cartDescriptions = Object.values(cartData).map(item => item.foodDescription);
-                        if (isMounted) {
-                            setCartFoods(cartDescriptions);
-                        }
-                    }
-
-                    if (snapshot3.exists()) {
-                        const orderData = snapshot3.val();
-                        orderDescriptions = Object.values(orderData).map(item => item.foodDescription);
-                        if (isMounted) {
-                            setOrderFoods(orderDescriptions);
-                        }
-                    }
-
-                    const allDescriptions = [...new Set([...cartDescriptions, ...orderDescriptions])];
-
-                    // Extract keywords from all descriptions
-                    const keywords = allDescriptions.reduce((acc, desc) => {
-                        const words = desc.split(' ');
-                        words.forEach(word => {
-                            if (!acc.includes(word.toLowerCase())) {
-                                acc.push(word.toLowerCase());
-                            }
-                        });
-                        return acc;
-                    }, []);
-
-                    const matchingFoods = allFoods.filter(food =>
-                        keywords.some(keyword => food.description.toLowerCase().includes(keyword))
-                    );
-
-                    const randomSelectedFoods = [];
-                    const randomIndexes = [];
-
-                    // Prioritize matching foods
-                    while (randomSelectedFoods.length < 8 && matchingFoods.length > randomSelectedFoods.length) {
-                        const randomIndex = Math.floor(Math.random() * matchingFoods.length);
-                        if (!randomIndexes.includes(randomIndex)) {
-                            randomIndexes.push(randomIndex);
-                            randomSelectedFoods.push(matchingFoods[randomIndex]);
-                        }
-                    }
-
-                    // If not enough matching foods, fill with other random foods
-                    if (randomSelectedFoods.length < 8) {
-                        const remainingFoods = allFoods.filter(food => !randomSelectedFoods.includes(food));
-                        while (randomSelectedFoods.length < 8) {
-                            const randomIndex = Math.floor(Math.random() * remainingFoods.length);
-                            if (!randomSelectedFoods.includes(remainingFoods[randomIndex])) {
-                                randomSelectedFoods.push(remainingFoods[randomIndex]);
+                        if (snapshot2.exists()) {
+                            const cartData = snapshot2.val();
+                            cartDescriptions = Object.values(cartData).map(item => item.foodDescription);
+                            if (isMounted) {
+                                setCartFoods(cartDescriptions);
                             }
                         }
-                    }
 
-                    if (isMounted) {
-                        setRandomFoods(randomSelectedFoods);
-                        setDataLoaded(true);
+                        if (snapshot3.exists()) {
+                            const orderData = snapshot3.val();
+                            orderDescriptions = Object.values(orderData).map(item => item.foodDescription);
+                            if (isMounted) {
+                                setOrderFoods(orderDescriptions);
+                            }
+                        }
+
+                        const allDescriptions = [...new Set([...cartDescriptions,...orderDescriptions])];
+                        const keywords = allDescriptions.reduce((acc, desc) => {
+                            const words = desc.split(' ');
+                            words.forEach(word => {
+                                if (!acc.includes(word.toLowerCase())) {
+                                    acc.push(word.toLowerCase());
+                                }
+                            });
+                            return acc;
+                        }, []);
+
+                        const matchingFoods = allFoods.filter(food =>
+                            keywords.some(keyword => food.description.toLowerCase().includes(keyword))
+                        );
+
+                        const randomSelectedFoods = [];
+                        const randomIndexes = [];
+                        while (randomSelectedFoods.length < 8 && matchingFoods.length > randomSelectedFoods.length) {
+                            const randomIndex = Math.floor(Math.random() * matchingFoods.length);
+                            if (!randomIndexes.includes(randomIndex)) {
+                                randomIndexes.push(randomIndex);
+                                randomSelectedFoods.push(matchingFoods[randomIndex]);
+                            }
+                        }
+                        if (randomSelectedFoods.length < 8) {
+                            const remainingFoods = allFoods.filter(food =>!randomSelectedFoods.includes(food));
+                            while (randomSelectedFoods.length < 8) {
+                                const randomIndex = Math.floor(Math.random() * remainingFoods.length);
+                                if (!randomSelectedFoods.includes(remainingFoods[randomIndex])) {
+                                    randomSelectedFoods.push(remainingFoods[randomIndex]);
+                                }
+                            }
+                        }
+                      
+                        if (isMounted) {
+                            setRandomFoods(randomSelectedFoods);
+                            setDataLoaded(true);
+                        }
+                    } else {
+                        console.log('No data available');
                     }
                 } else {
-                    console.log('No data available');
+                    if (snapshot.exists()) {
+                        const allDishes = snapshot.val();
+                        const allFoods = Object.keys(allDishes).reduce((acc, category) => [
+                           ...acc,
+                           ...allDishes[category].items,
+                        ], []);
+
+                        const randomSelectedFoods = [];
+                        while (randomSelectedFoods.length < 8) {
+                            const randomIndex = Math.floor(Math.random() * allFoods.length);
+                            if (!randomSelectedFoods.includes(allFoods[randomIndex])) {
+                                randomSelectedFoods.push(allFoods[randomIndex]);
+                            }
+                        }
+
+                        if (isMounted) {
+                            setRandomFoods(randomSelectedFoods);
+                            setDataLoaded(true);
+                        }
+                    } else {
+                        console.log('No data available');
+                    }
                 }
             } catch (error) {
                 console.error('Error fetching data:', error);
@@ -116,120 +135,3 @@ const useFetchRandomFoods = (user) => {
 };
 
 export default useFetchRandomFoods;
-
-// import { useState, useEffect } from 'react';
-// import { get, ref } from 'firebase/database';
-// import { database } from 'components/LoginSignUp/firebaseConfig';
-
-// const useFetchRandomFoods = (user) => {
-//     const [randomFoods, setRandomFoods] = useState([]);
-//     const [dataLoaded, setDataLoaded] = useState(false);
-//     const [cartFoods, setCartFoods] = useState([]);
-//     const [orderFoods, setOrderFoods] = useState([]);
-  
-//     useEffect(() => {
-//       let isMounted = true;
-  
-//       const fetchRandomFoods = async () => {
-//         try {
-//           if (!database) {
-//             console.error("Database reference is not available");
-//             return;
-//           }
-  
-//           const dishesRef = ref(database, 'Dishes');
-//           const snapshot = await get(dishesRef);
-  
-//           const descCartsRef = ref(database, `carts/${user.uid}`);
-//           const snapshot2 = await get(descCartsRef);
-  
-//           const descOrdersRef = ref(database, `transactions/${user.uid}`);
-//           const snapshot3 = await get(descOrdersRef);
-  
-//           if (snapshot.exists()) {
-//             const allDishes = snapshot.val();
-//             const allFoods = Object.keys(allDishes).reduce((acc, category) => [
-//               ...acc,
-//               ...allDishes[category].items,
-//             ], []);
-  
-//             let cartDescriptions = [];
-//             let orderDescriptions = [];
-  
-//             if (snapshot2.exists()) {
-//               const cartData = snapshot2.val();
-//               cartDescriptions = Object.values(cartData).map(item => item.foodDescription);
-//               if (isMounted) {
-//                 setCartFoods(cartDescriptions);
-//               }
-//             }
-  
-//             if (snapshot3.exists()) {
-//               const orderData = snapshot3.val();
-//               orderDescriptions = Object.values(orderData).map(item => item.foodDescription);
-//               if (isMounted) {
-//                 setOrderFoods(orderDescriptions);
-//               }
-//             }
-  
-//             const allDescriptions = [...new Set([...cartDescriptions, ...orderDescriptions])];
-  
-//             const keywords = allDescriptions.reduce((acc, desc) => {
-//               if (typeof desc === 'string') {
-//                 const words = desc.toLowerCase().split(' ');
-//                 words.forEach(word => {
-//                   if (!acc.includes(word)) {
-//                     acc.push(word);
-//                   }
-//                 });
-//               }
-//               return acc;
-//             }, []);
-  
-//             const matchingFoods = allFoods.filter(food =>
-//               keywords.some(keyword => food.description.toLowerCase().includes(keyword))
-//             );
-  
-//             const randomSelectedFoods = [];
-//             const randomIndexes = [];
-  
-//             while (randomSelectedFoods.length < 8 && matchingFoods.length > randomSelectedFoods.length) {
-//               const randomIndex = Math.floor(Math.random() * matchingFoods.length);
-//               if (!randomIndexes.includes(randomIndex)) {
-//                 randomIndexes.push(randomIndex);
-//                 randomSelectedFoods.push(matchingFoods[randomIndex]);
-//               }
-//             }
-  
-//             if (randomSelectedFoods.length < 8) {
-//               const remainingFoods = allFoods.filter(food => !randomSelectedFoods.includes(food));
-//               while (randomSelectedFoods.length < 8 && remainingFoods.length > 0) {
-//                 const randomIndex = Math.floor(Math.random() * remainingFoods.length);
-//                 randomSelectedFoods.push(remainingFoods[randomIndex]);
-//                 remainingFoods.splice(randomIndex, 1);
-//               }
-//             }
-  
-//             if (isMounted) {
-//               setRandomFoods(randomSelectedFoods);
-//               setDataLoaded(true);
-//             }
-//           } else {
-//             console.log('No data available');
-//           }
-//         } catch (error) {
-//           console.error('Error fetching data:', error);
-//         }
-//       };
-  
-//       fetchRandomFoods();
-  
-//       return () => {
-//         isMounted = false;
-//       };
-//     }, [user]);
-  
-//     return { randomFoods, dataLoaded, cartFoods, orderFoods };
-//   };
-
-// export default useFetchRandomFoods;
