32affc953da7018f538257f5c7e9eb5e150c02e8
Sagar Thakur <86216787+ravansagar@users.noreply.github.com>
2024-07-08 16:46:51 +0545

    Update firebaseConfig.jsx
    Added VITE_ before enviroment name

--- src/components/LoginSignUp/firebaseConfig.jsx
+++ src/components/LoginSignUp/firebaseConfig.jsx
@@ -5,13 +5,13 @@ import { getDatabase } from "firebase/database";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.FIREBASE_API_KEY,
-    authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.FIREBASE_APP_ID,
-    measurementId: import.meta.env.FIREBASE_MEASURMENT_ID,
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASURMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
